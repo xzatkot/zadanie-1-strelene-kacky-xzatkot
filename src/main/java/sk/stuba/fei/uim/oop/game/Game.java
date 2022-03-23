c0e97e98c9df4d061c178b6ce85fc74e94fef537
xzatkot <xzatkot@stuba.sk>
2022-03-23 19:17:27 +0100

    added some essential methods and extended Game() method

--- src/main/java/sk/stuba/fei/uim/oop/game/Game.java
+++ src/main/java/sk/stuba/fei/uim/oop/game/Game.java
@@ -1,24 +1,50 @@
 package sk.stuba.fei.uim.oop.game;
 
+import sk.stuba.fei.uim.oop.tiles.Duck;
+import sk.stuba.fei.uim.oop.tiles.Water;
+import sk.stuba.fei.uim.oop.tiles.Tile;
 import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
 import sk.stuba.fei.uim.oop.player.Player;
 import sk.stuba.fei.uim.oop.tiles.card.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Game {
-    //public Duck[] pond = {null};
+    public ArrayList<Tile> pond = new ArrayList<>();
+    public ArrayList<Tile> tileCardsStack = new ArrayList<>();
     public ArrayList<Card> cardStack = new ArrayList<>();
-    public boolean[] crosshairArray = {false};
+    public boolean[] crosshairArray = {false, false, false, false, false, false};
+
     public Game() {
         System.out.println("**** Strelene kacky ****");
         int numOfPlayers = ZKlavesnice.readInt("Zadaj pocet hracov: ");
     }
+
     public boolean getBoolValue(int index){
         return crosshairArray[index];
     }
     public void setBoolValue(int index){
         crosshairArray[index] = true;
     }
+
+    public void initialiseTileCards(int numOfPlayers){
+        for (int i=0;i<numOfPlayers;i++){
+            for (int j=0;j<5;j++) {
+                this.tileCardsStack.add(new Duck(i+1));
+            }
+        }
+        for (int i=0;i<5;i++){
+            this.tileCardsStack.add(new Water());
+        }
+        Collections.shuffle(this.tileCardsStack);
+    }
+
+    public void startPond(){
+        for (int i=0;i<6;i++){
+            this.pond.add(this.tileCardsStack.get(i));
+        }
+    }
+
     public void createCardStack(){
         for (int i=0;i<10;i++){
             this.cardStack.add(new Aim());
@@ -37,5 +63,6 @@ public class Game {
             this.cardStack.add(new Roshambo());
         }
         this.cardStack.add(new DuckDance());
+        Collections.shuffle(this.cardStack);
     }
 }
